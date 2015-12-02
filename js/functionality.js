b5c72209ef0b96d12f03628719cd73987d349cec
alex-B-Ly <abl12582@gmail.com>
2015-12-02 01:00:23 -0500

    Manipulated JS object to extract useful data.

--- js/functionality.js
+++ js/functionality.js
@@ -1,19 +1,49 @@
 $(document).ready(function() {
 
   // TWEET INFO
-  function tweetInfo(){
+  function userInfo(){
     for(var i=0; i<window.alldata.length; i++){
+      // DUPLICATE USER CHECK
+      if($('#main-content').children().hasClass(alldata[i].user.name)){
+        return;
+      }
       // ROW CREATE
       var newRow = $('<div>');
-      newRow.addClass('row').addClass('text-center').addClass('tweet' + alldata[i].id_str);
+      newRow.addClass('row').addClass('text-center').addClass(alldata[i].user.name);
 
-      // TITLE CREATE
+      // USER NAME CREATE
       var tweetTitle = $('<div>');
-      tweetTitle.addClass('col-xs-12 tweet-title');
-      tweetTitle.text('Tweet #' + alldata[i].id_str);
+      tweetTitle.addClass('col-xs-12').addClass('text-center').addClass('user-name');
+      tweetTitle.text('User: ' + alldata[i].user.name);
       newRow.append(tweetTitle);
 
-      // USER INFO
+      // ==== USER INFO =====
+      var userId = $('<div>'),
+      userLocation = $('<div>'),
+      userImg = $('<div>'),
+      userStatuses = $('<div>'),
+      userFriends = $('<div>');
+
+      userId.addClass('col-xs-2').addClass('user-id');
+      userId.text('ID: ' + alldata[i].user.id_str);
+
+      userLocation.addClass('col-xs-3').addClass('user-location');
+      userLocation.text('Location: ' + alldata[i].user.location);
+
+      userImg.addClass('col-xs-7').addClass('user-image');
+      userImg.text('Image: ' + alldata[i].user.profile_image_url);
+
+      userStatuses.addClass('col-xs-6').addClass('user-statuses');
+      userStatuses.text('Statuses: '+alldata[i].user.statuses_count);
+
+      userFriends.addClass('col-xs-6').addClass('user-friends');
+      userFriends.text('Friends: '+alldata[i].user.friends_count);
+
+      newRow.append(userId);
+      newRow.append(userLocation);
+      newRow.append(userImg);
+      newRow.append(userStatuses);
+      newRow.append(userFriends);
 
       // APPEND NEW ROW
       $('#main-content').append(newRow);
@@ -21,6 +51,5 @@ $(document).ready(function() {
   }
 
   // FUNCTIONS CALLED
-  tweetInfo();
-
+  userInfo();
 });
